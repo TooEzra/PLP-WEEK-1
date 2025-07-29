b2def0ed41bc0dcf5ea49deb99eddad2822a527b
Ezra Kipkoech <147641185+TooEzra@users.noreply.github.com>
2025-07-29 11:58:02 +0300

    Add files via upload

---
+++ PYTHON/week 1/data_types.py
@@ -0,0 +1,35 @@
+#numeric data types
+num1 = 55
+num2 = 5.3
+print(num1)
+print(num2)
+
+#list data type
+languages = ["Python", "Dart", "Web", 23]
+print(languages)
+
+#list items
+languages = ["Python", "Dart", "Web", 23]
+print(languages[1]) # prints Dart
+print(languages[2]) # prints Web
+print(languages[3])  # prints 23
+print(languages[0])  # prints Python
+
+#tuple data type
+products = ('XBox', 499.99, "Habibi", 23)
+print(products)
+
+#access tuple
+products = ('XBox', 499.99, "Habibi", 23)
+print(products[2])
+print(products[0])
+print(products[1])  # prints 499.99
+print(products[3])  # prints 23
+
+#set data type
+student_ids = {112, 114, 117, 113}
+print(student_ids)
+
+#dictionary data type
+capital_city = {"Kenya": "Nairobi", "Nigeria": "Lagos"}
+print(capital_city)
---
+++ PYTHON/week 1/index.py
@@ -0,0 +1,23 @@
+site_name = "Power Learn Project"
+print(site_name)
+
+site_name = "Power Learn Project"
+print(site_name)
+
+# Assigning new value to site_name
+site_name = "I love coding 😊"
+print(site_name)
+
+# The output will be:
+# Power Learn Project
+# I love coding 😊
+
+a,b,c = 5, 7, "Hello world"
+print(a) # prints 5
+print(b) # prints 5
+print(c) # prints Hello World
+
+num = 55
+Num = 510
+print(num) #5
+print(Num) #510
